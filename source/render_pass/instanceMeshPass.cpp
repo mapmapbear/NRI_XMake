0090397d4ecd2b6fc09498918c8044f82711446c
mapmapbear <m.n.jhop@163.com>
2025-03-27 20:50:43 +0800

    add simple mesh draw pass

--- source/render_pass/instanceMeshPass.cpp
+++ source/render_pass/instanceMeshPass.cpp
@@ -163,7 +163,7 @@ void InstanceMeshPass::BindMemory() {
 		nri::BufferViewDesc bufferViewDesc = {};
 		bufferViewDesc.buffer = m_ConstantBuffer;
 		bufferViewDesc.viewType = nri::BufferViewType::CONSTANT;
-		bufferViewDesc.offset = constantBufferSize;
+		bufferViewDesc.offset = 0;
 		bufferViewDesc.size = constantBufferSize;
 		NRI_ABORT_ON_FAILURE(
 				NRI.CreateBufferView(bufferViewDesc, m_ConstantBufferView));
@@ -354,8 +354,30 @@ void InstanceMeshPass::BuildPipeline() {
 }
 
 void InstanceMeshPass::Render(RenderInfo &info, Camera &camera) {
-	vec3 cameraPos = camera.state.globalPosition;
 	auto NRI = *m_NRI;
+
+	ConstantBufferLayout *commonConstants = (ConstantBufferLayout *)NRI.MapBuffer(
+			*m_ConstantBuffer, 0,
+			sizeof(ConstantBufferLayout));
+
+	const glm::mat4 m1 = glm::rotate(glm::mat4(1.0f), glm::radians(90.0f),
+			glm::vec3(1.0f, 0.f, 0.f));
+	const glm::mat4 m2 = glm::rotate(glm::mat4(1.0f), (float)glfwGetTime(),
+			glm::vec3(0.0f, 1.f, 0.f));
+	glm::mat4 m = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -0.8f, 0.0f)) * m2 * m1;
+	// const glm::mat4 p = glm::perspectiveLH_ZO(glm::radians(m_Fov), 900.f / 600.f, 0.1f, 100.0f);
+	const glm::mat4 p = camera.state.mViewToClip;
+	const glm::vec3 cameraPos = camera.state.globalPosition;
+	glm::vec3 target = cameraPos + glm::vec3(camera.state.mWorldToView[0][2], camera.state.mWorldToView[1][2], camera.state.mWorldToView[2][2]);
+	const glm::mat4 v = glm::lookAtLH(cameraPos, target, glm::vec3(0.0f, 1.0f, 0.0f));
+
+	if (commonConstants) {
+		commonConstants->modelMat = m;
+		commonConstants->viewMat = camera.state.mWorldToView;
+		commonConstants->projectMat = p;
+		NRI.UnmapBuffer(*m_ConstantBuffer);
+	}
+
 	{
 		helper::Annotation annotation(NRI, info.cmdBuffer, "SimpleMeshTEST");
 
