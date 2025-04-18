14091703b203f0817cfcb8a2c5e409207771b820
mapmapbear <m.n.jhop@163.com>
2025-04-18 16:09:55 +0800

    add gflags bat file

---
+++ StartGflags.bat
@@ -0,0 +1,9 @@
+
+set gflags_10_path="C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\gflags.exe"
+set gflags_8_path="C:\Program Files (x86)\Windows Kits\8.1\Debuggers\x64\gflags.exe"
+
+if exist %gflags_10_path% (         
+	call %gflags_10_path% /p /enable DemoApp.exe  /full
+) else (  
+	call %gflags_10_path% /p /enable DemoApp.exe /full
+)
