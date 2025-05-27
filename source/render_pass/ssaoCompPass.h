29ae2e56129949cc01186745776dc34588778af7
mapmapbear <m.n.jhop@163.com>
2025-05-27 20:59:36 +0800

    add ssao header file

---
+++ source/render_pass/ssaoCompPass.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "CommonRenderPass.h"
+
+struct PushConstants
+{
+    uint32_t texDepth;
+    uint32_t texRotation;
+    uint32_t texOut;
+    uint32_t smpl;
+    float zNear;
+    float zFar;
+    float radius;
+    float attScale;
+    float distScale;
+};
+
+class Texture;
+class SSAOCompPass : public CommonRenderPass {
+public:
+	SSAOCompPass(Renderer *renderer);
+	void Render(struct RenderInfo &info, Camera &camera) override;
+	void BuildPipeline() override;
+	void AllocGPUMemory() override;
+	void BindMemory() override;
+
+private:
+	nri::PipelineLayout *m_SSAOPipelineLayout = nullptr;
+	nri::Pipeline *m_SSAOPipeline = nullptr;
+
+	nri::Texture *m_DepthTexture = nullptr;
+	nri::Descriptor *m_DepthTextureShaderResource = nullptr;
+	nri::Texture *m_NormalTexture = nullptr;
+	nri::Descriptor *m_NormalTextureShaderResource = nullptr;
+	nri::Texture *m_NoiseTexture = nullptr;
+	utils::Texture m_noiseTexture;
+	nri::Descriptor *m_NoiseTextureShaderResource = nullptr;
+	nri::Descriptor *m_SSAOTextureShaderResource = nullptr;
+	nri::Descriptor *m_Sampler = nullptr;
+	nri::DescriptorSet *m_SSAOTextureDescriptorSet = nullptr;
+
+	std::shared_ptr<Texture> m_SSAOTexture = nullptr;
+	std::shared_ptr<Texture> m_RotationTexture = nullptr;
+};
